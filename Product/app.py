c393aa5b8f6fd4e4f2804b87a535f89f11f04908
gunavc <97451210+gunavc@users.noreply.github.com>
2024-04-05 13:08:41 +0530

    URL fix

--- Product/app.py
+++ Product/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ecommerce"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Ecommerce"
 mongo = PyMongo(app)
 
 @app.route('/products', methods=['POST'])
