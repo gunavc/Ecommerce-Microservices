d7b48d7534ab5ee3ee763b2f2cde97b95a8350af
gunavc <97451210+gunavc@users.noreply.github.com>
2024-04-05 13:33:07 +0530

    Changes to work with docker

--- Order/app.py
+++ Order/app.py
@@ -1,19 +1,43 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+import os
+import pymongo.errors
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ecommerce"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost:27017/ecommerce")
 mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return jsonify("Welcome to orders")
+
+@app.route("/check")
+def check():
+    a = 0
+    try:
+        mongo.db.command('ping')
+        print("MongoDB Connection Successful!")
+        a = 1
+    except pymongo.errors.ConnectionFailure:
+        print("MongoDB Connection Failed!")
+    return jsonify(a)
+
 
 @app.route('/orders', methods=['POST'])
 def create_order():
     orders = mongo.db.orders
     data = request.get_json()
-    user_id = data.get('user_id')
+    userid = data.get('userid')
     products = data.get('products')
+    
+    min_id = 0
+    res = orders.find().sort({"orderid":-1}).limit(1)
+    for doc in res:
+        min_id = int(doc["orderid"])
 
     order = {
-        'user_id': user_id,
+        'orderid': min_id + 1,
+        'userid': userid,
         'products': products,
     }
     result = orders.insert_one(order)
@@ -22,12 +46,12 @@ def create_order():
     print("Order Successfully Added")
     return jsonify(order), 201
 
-@app.route('/orders/<order_id>', methods=['GET'])
+@app.route('/orders/<int:order_id>', methods=['GET'])
 def get_order(order_id):
     orders = mongo.db.orders
-    order = orders.find_one({'_id': order_id})
+    order = orders.find_one({'orderid': order_id}, {"_id":0})
     if order:
-        order['_id'] = str(order['_id'])
+        order['orderid'] = str(order['orderid'])
         return jsonify(order)
     else:
         return jsonify({'error': 'Order not found'}), 404
