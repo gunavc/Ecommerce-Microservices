a6ef6486d483ae50c5449b43e7ecdbadd8fe7f15
gunavc <97451210+gunavc@users.noreply.github.com>
2024-04-05 13:22:22 +0530

    docker files

--- Product/app.py
+++ Product/app.py
@@ -1,9 +1,26 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
+import pymongo.errors
+import os 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/Ecommerce"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI" ,"mongodb://localhost:27017/Ecommerce")
 mongo = PyMongo(app)
+
+@app.route("/")
+def hello():
+    return jsonify("Welcome to products")
+
+@app.route('/check')
+def check():
+    a = 0
+    try:
+        mongo.db.command('ping')
+        print("MongoDB Connection Successful!")
+        a = 1
+    except pymongo.errors.ConnectionFailure:
+        print("MongoDB Connection Failed!")
+    return jsonify(a)
 
 @app.route('/products', methods=['POST'])
 def create_product():
@@ -21,7 +38,7 @@ def create_product():
     product['_id'] = str(product['_id'])
     return jsonify(product), 201
 
-@app.route('/products', methods=['GET'])
+@app.route('/getproducts', methods=['GET'])
 def get_products():
     products = mongo.db.products
     product_list = [{'_id': str(product['_id']), 'name': product['name'], 'price': product['price']} for product in products.find()]
