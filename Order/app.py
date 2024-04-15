0b2faad8845250c786e569c4c79ce8600b6459dc
gunavc <97451210+gunavc@users.noreply.github.com>
2024-04-15 09:42:28 +0530

    fixed url issue

--- Order/app.py
+++ Order/app.py
@@ -4,7 +4,8 @@ import os
 import pymongo.errors
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost:27017/ecommerce")
+app.config["MONGO_URI"] = "mongodb://mongo:27017/Ecommerce"
+#app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost:27017/ecommerce")
 mongo = PyMongo(app)
 
 @app.route('/')
